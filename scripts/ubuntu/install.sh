f5836fc6bea9ae1874f5aea178a56c8e5d18d639
zmad5306 <zmad5306@outlook.com>
2021-10-30 08:59:13 -0400

    better organize scripts

--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -8,6 +8,6 @@ rm -rf node-client-1
 git clone https://github.com/zmad5306/node-client-1.git &&
 cd node-client-1 &&
 npm i &&
-sudo cp node-cliet-1.service /etc/systemd/system/node-client-1.service &&
+sudo cp ./services/ubuntu/node-cliet-1.service /etc/systemd/system/node-client-1.service &&
 sudo systemctl enable node-client-1.service &&
 sudo systemctl start node-client-1.service
