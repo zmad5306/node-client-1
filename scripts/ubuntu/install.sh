c1534f44accd232462ed1ff845371358b318c82f
zmad5306 <zmad5306@outlook.com>
2021-10-31 12:30:26 -0400

    pinned to pwd

--- scripts/ubuntu/install.sh
+++ scripts/ubuntu/install.sh
@@ -4,9 +4,9 @@ sudo apt update
 sudo apt upgrade -y
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 sudo apt-get install -y nodejs
-rm -rf node-client-1
+rm -rf ./node-client-1
 git clone https://github.com/zmad5306/node-client-1.git
-cd node-client-1
+cd ./node-client-1
 npm i
 sudo cp ./services/ubuntu/node-cliet-1.service /etc/systemd/system/node-client-1.service
 sudo systemctl enable node-client-1.service
